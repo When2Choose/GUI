65c86bfb1541c7e376885c97039db734eefa0454
agalgano08 <agalgano@wpi.edu>
2020-11-11 15:42:35 -0500

    Atom sucks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './App.css';
 import CreateChoice from "./CreateChoice";
-
+import Menu from "./Menu";
 
 function App() {
   return (
     <div className="App">
-        
+      <Menu/>
       <CreateChoice/>
+
     </div>
   );
 }
--- src/Menu.js
+++ src/Menu.js
@@ -1,13 +1,18 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
 
 function Menu() {
     return (
         <div className="Menu">
-            <AppBar id = "MenuBar" position = "static">
-
-
-
+            <AppBar id="MenuBar" position="static">
+                <Toolbar id="toolbar">
+                    <Button variant="zoomUp" id="Create" color="primary">Zoom Up </Button>
+                    <Button variant="newChoice" id="Create" color="primary">Create New Choice</Button>
+                    <p> ID </p>
+                </Toolbar>
+            </AppBar>
         </div>
     );
 }
