f2dab504a78ab63283724c5bac2165ed36a01f39
agalgano08 <agalgano@wpi.edu>
2020-11-11 15:26:39 -0500

    all set

--- src/Menu.js
+++ src/Menu.js
@@ -1,8 +1,14 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar"
 
 function Menu() {
     return (
-        <div className="Menu"></div>
+        <div className="Menu">
+            <AppBar id = "MenuBar" position = "static">
+
+
+
+        </div>
     );
 }
 
