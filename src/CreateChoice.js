469fcf87a0960c94d8934d46e6ffb63032081c09
GrantPerkins <gcperk20@gmail.com>
2020-11-11 15:02:31 -0500

    Initial commit

---
+++ src/CreateChoice.js
@@ -0,0 +1,26 @@
+import React from "react";
+import TextField from '@material-ui/core/TextField';
+import Button from "@material-ui/core/Button";
+import "./CreateChoice.css"
+
+function CreateChoice() {
+    return (
+        <div className="CreateChoice">
+            <div className="CreateChoiceContent">
+                <div className="Vertical">
+                    <TextField id="choiceAllowedMembersNumber" type="number"
+                               label="Number of group members" style={{width: '25%'}} />
+                </div>
+                <div className="Vertical">
+                    <TextField id="choiceDescription" type="text"
+                               label="Description" style={{width: '75%'}} multiline={true}/>
+                </div>
+                <div className="Vertical">
+                    <Button variant="contained" id="Create" color="primary">Create choice</Button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CreateChoice;
