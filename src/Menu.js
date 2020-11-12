601ec50f3491936e6e847151429e004183aadecc
GrantPerkins <gcperk20@gmail.com>
2020-11-11 23:29:01 -0500

    Clean up menu

--- src/Menu.js
+++ src/Menu.js
@@ -2,15 +2,23 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
+import "./Menu.css";
 
 function Menu() {
     return (
         <div className="Menu">
             <AppBar id="MenuBar" position="static">
-                <Toolbar id="toolbar">
-                    <Button variant="zoomUp" id="Create" color="primary">Zoom Up </Button>
-                    <Button variant="newChoice" id="Create" color="primary">Create New Choice</Button>
-                    <p> ID </p>
+                <Toolbar id="toolbar" style={{display:"flex", justifyContent:"space-between"}}>
+                    <div className="LeftMenu">
+
+                        <Button variant="contained" id="Scroll" color="default">Scroll Up </Button>
+                        <span style={{width: 10}}/>
+                        <Button variant="contained" id="Create" color="default">Create New Choice</Button>
+                    </div>
+                    <div className="RightMenu">
+                        <Button variant="contained" id="Create" color="default">FIRST NAME HERE</Button>
+                    </div>
+
                 </Toolbar>
             </AppBar>
         </div>
