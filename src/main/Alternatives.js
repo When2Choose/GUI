28c2fb265ecddf91f84b302ec173f378007d8502
GrantPerkins <gcperk20@gmail.com>
2020-11-12 23:32:51 -0500

    Alternatives

--- src/main/Alternatives.js
+++ src/main/Alternatives.js
@@ -1,8 +1,66 @@
 import React from "react";
+import PropTypes from 'prop-types';
+import {Button, Typography} from "@material-ui/core";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import "./Alternative.css"
+import CardContent from "@material-ui/core/CardContent";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
 
-function Alternative() {
+function renderRow(props) {
+    const { index, style } = props;
+
     return (
-        <div/>
+        <ListItem button style={style} key={index}>
+            <ListItemText primary={`Item ${index + 1}`} />
+        </ListItem>
+    );
+}
+
+renderRow.propTypes = {
+    index: PropTypes.number.isRequired,
+    style: PropTypes.object.isRequired,
+};
+
+function Alternative(props) {
+    return (
+        <Card variant="outlined" style={{paddingBottom: 10}}>
+            <CardContent>
+                <div className="Alternative">
+                    <div className="AlternativeHeading">
+                        <Typography variant="h5">Alternative {props.number}</Typography>
+                    </div>
+                    <div className="AlternativeDescription">
+                        <Typography variant="p">you know how it be, lorum ipsum dolor in da
+                            house. I need more text here, so I'm going to start telling you all about Arun. Arun is a
+                            guy
+                            who does a lot of stuff, like coding and taking classes and courses and lots of of teaching
+                            things at WPI</Typography>
+                    </div>
+                    <div className="ApprovalContent">
+                        <Grid container spacing={3} style={{paddingLeft: "1%", paddingRight: "1%"}}>
+                            <Grid item xs={6}>
+                                <Button variant="contained" id="Approve" color="primary"
+                                        style={{float: "right"}}>Approve</Button>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Button variant="contained" id="Disapprove" color="primary" style={{float: "left"}}>Disapprove</Button>
+                            </Grid> 
+                            <Grid item xs={6} id="approvers">
+                                {/*<FixedSizeList height={400} width={300} itemSize={46} itemCount={200}>*/}
+                                {/*    {renderRow}*/}
+                                {/*</FixedSizeList>*/}
+                            </Grid>
+                            <Grid item xs={6} id="dispprovers">
+                            </Grid>
+                        </Grid>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+
     );
 }
 
--- src/main/ViewChoice.js
+++ src/main/ViewChoice.js
@@ -1,30 +1,46 @@
 import React from "react";
 import {Button, Typography} from "@material-ui/core";
+import Card from '@material-ui/core/Card';
 import Alternative from "./Alternatives";
 import "./ViewChoice.css";
+import Grid from "@material-ui/core/Grid";
+import CardContent from "@material-ui/core/CardContent";
 
 function ViewChoice() {
     return (
         <div className="ViewChoice">
-            <div className="ChoiceHeadingContent">
-                <Typography variant="h2">Choice</Typography>
-                <Jumbotron>
-
-                </Jumbotron>
-                <div className="ChoiceDescriptionContent">
+            <Grid container spacing={3} style={{paddingLeft: "1%", paddingRight: "1%"}}>
+                <Grid item xs={12}>
+                    <Typography variant="h2">Choice</Typography>
+                </Grid>
+                <Grid item xs={9}>
                     <Typography variant="p" id="choiceDescription" style={{width: "100%"}}>Insert a lot of text here,
                         you know how it be, lorum ipsum dolor in da
                         house. I need more text here, so I'm going to start telling you all about Arun. Arun is a guy
                         who does a lot of stuff, like coding and taking classes and courses and lots of of teaching
                         things at WPI</Typography>
-                </div>
-                <div className="CompleteChoice">
-                    <Button variant="contained" id="Complete" color="primary">Complete</Button>
-                </div>
-            </div>
-            <div className="ChoiceAlternatives">
-                <Alternative/>
-            </div>
+                </Grid>
+                <Grid item xs={3}>
+                    <Button variant="contained" id="Complete" color="primary" style={{float: "right"}}>Complete</Button>
+                </Grid>
+
+                <Grid item xs={12} id="alt0">
+                    <Alternative number="0"/>
+                </Grid>
+
+                <Grid item xs={12} id="alt1">
+                    <Alternative number="1"/>
+                </Grid>
+                <Grid item xs={12} id="alt2">
+                    <Alternative number="2"/>
+                </Grid>
+                <Grid item xs={12} id="alt3">
+                    <Alternative number="3"/>
+                </Grid>
+                <Grid item xs={12} id="alt4">
+                    <Alternative number="4"/>
+                </Grid>
+            </Grid>
         </div>
     );
 }
