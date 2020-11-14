193c50cd8962387aa69af9ee21e06033b6df921c
GrantPerkins <gcperk20@gmail.com>
2020-11-14 15:14:44 -0500

    Dark theme

--- src/main/App.js
+++ src/main/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 import {
     Switch,
     Route,
@@ -11,29 +12,45 @@ import CreateChoice from "./CreateChoice";
 import Menu from "./Menu";
 import Login from "./Login";
 import ViewChoice from "./ViewChoice";
+import {ThemeProvider} from "@material-ui/styles";
+import {useMediaQuery, createMuiTheme, CssBaseline} from "@material-ui/core";
 
 
 function App() {
     document.title = "Choice App";
-  return (
-    <div className="App">
-        <BrowserRouter>
-            <Menu/>
-            <Switch>
-                <Route path="/view">
-                    <ViewChoice/>
-                </Route>
-                <Route path="/create">
-                    <CreateChoice/>
-                </Route>
-                <Route path="/login">
-                    <Login/>
-                </Route>
-            </Switch>
-        </BrowserRouter>
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
-    </div>
-  );
+    const theme = React.useMemo(
+        () =>
+            createMuiTheme({
+                palette: {
+                    type: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div className="App">
+                <BrowserRouter>
+                    <Menu/>
+                    <Switch>
+                        <Route path="/view">
+                            <ViewChoice/>
+                        </Route>
+                        <Route path="/create">
+                            <CreateChoice/>
+                        </Route>
+                        <Route path="/login">
+                            <Login/>
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
