2421c2b9bc312e5208c45ec71316902e26ad01dd
GrantPerkins <gcperk20@gmail.com>
2020-11-14 15:44:06 -0500

    Remove unused import

--- src/main/Alternatives.js
+++ src/main/Alternatives.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from 'prop-types';
 import {Button, Typography} from "@material-ui/core";
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
