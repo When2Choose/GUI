89878a74100de2323201f9093f3058ca778b52a2
GrantPerkins <gcperk20@gmail.com>
2020-11-19 18:41:45 -0500

    New login page

--- src/main/Login.js
+++ src/main/Login.js
@@ -1,42 +1,85 @@
 import React from "react";
-import {Button, Typography, Input, FormControl, Grid, Paper} from "@material-ui/core";
-import {Link} from "react-router-dom";
+import {
+    Button,
+    Typography,
+    Grid,
+    Paper,
+    TextField
+} from "@material-ui/core";
 import "./Login.css";
 
 function Login() {
     return (
         <div className="LoginContent">
-            <div className="LeftLoginContent">
-                <div className="LoginList">
-                    <form className="LoginUser">
-                        <Typography variant="h4"> User </Typography>
-                        <Input required id="UserName" placeholder="Name" style={{width: '75%', padding: '5px'}}/>
-                        <Input id="UserPassword" type="password"
-                               placeholder="Password (Optional)" style={{width: '75%', padding: '5px'}}/>
-                        <Input required id="ChoiceID" type="number"
-                               placeholder="Choice ID" style={{width: '75%', padding: '5px'}}/>
-                        <div className="Login">
-                            <Button variant="contained" id="Login" color="primary" size="large" type="submit">Login
-                                User</Button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div className="RightLoginContent">
-                <div className="LoginList">
-                    <form className="loginAdmin">
-                        <Typography variant="h4"> Admin </Typography>
-                        <Input required id="AdminName" placeholder="Name" style={{width: '75%', padding: '5px'}}/>
-                        <Input required id="AdminPassword" type="password"
-                               placeholder="Password (Optional)" style={{width: '75%', padding: '5px'}}/>
-                        <div className="Login">
-                            <Button variant="contained" id="Login" color="primary" size="large" type="submit">Login
-                                Admin</Button>
-                        </div>
-                    </form>
-
-                </div>
-            </div>
+            <Grid container spacing={3} justify="center" direction="row" className="LoginGrid">
+                <Grid item xs={6}>
+                    <Grid container direction="column" spacing={2} className="LoginForm" justify="center">
+                        <Grid item>
+                            <Paper variant="elevation" elevation={2} className="Paper">
+                                <form className="LoginUser">
+                                    <Grid container direction="column" spacing={2}>
+                                        <Grid item>
+                                            <Typography variant="h4"> User </Typography>
+                                        </Grid>
+                                        <Grid item className="LoginInputs">
+                                            <Grid container direction="column" spacing={2}>
+                                                <Grid item>
+                                                    <TextField required variant="outlined" id="UserName" placeholder="Name" className="Input"/>
+                                                </Grid>
+                                                <Grid item>
+                                                    <TextField variant="outlined" id="UserPassword" type="password"
+                                                           placeholder="Password (Optional)"
+                                                           className="Input"/>
+                                                </Grid>
+                                                <Grid item>
+                                                    <TextField required variant="outlined" id="ChoiceID" type="number"
+                                                           placeholder="Choice ID" className="Input"/>
+                                                </Grid>
+                                            </Grid>
+                                        </Grid>
+                                        <Grid item>
+                                            <Button variant="contained" id="Login" color="primary" size="large"
+                                                    type="submit">Login User</Button>
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                    <Grid container direction="column" spacing={2} className="LoginForm" justify="center">
+                        <Grid item>
+                            <Paper variant="elevation" elevation={2} className="Paper">
+                                <form className="LoginAdmin">
+                                    <Grid container direction="column" spacing={2}>
+                                        <Grid item>
+                                            <Typography variant="h4"> Admin </Typography>
+                                        </Grid>
+                                        <Grid item className="LoginInputs">
+                                            <Grid container direction="column" spacing={2}>
+                                                <Grid item>
+                                                    <TextField required variant="outlined" id="AdminName" placeholder="Name"
+                                                           className="Input"/>
+                                                </Grid>
+                                                <Grid item>
+                                                    <TextField required variant="outlined" id="AdminPassword" type="password"
+                                                           placeholder="Password (Optional)"
+                                                           className="Input"/>
+                                                </Grid>
+                                            </Grid>
+                                        </Grid>
+                                        <Grid item>
+                                            <Button variant="contained" id="Login" color="primary" size="large"
+                                                    type="submit">Login Admin</Button>
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         </div>
     );
 }
