43e585d6c9e670e79d49c0c43e45a6057500b0be
GrantPerkins <gcperk20@gmail.com>
2020-12-01 21:23:15 -0500

    Clean up bad code

--- src/main/ViewChoice.js
+++ src/main/ViewChoice.js
@@ -4,30 +4,32 @@ import {
     Typography,
     Grid
 } from "@material-ui/core";
-import Alternative from "./Alternatives";
-import "./ViewChoice.css";
 import {
     Link,
     withRouter
 } from "react-router-dom";
+import Alternative from "./Alternatives";
+import "./ViewChoice.css";
 
 class ViewChoice extends React.Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             name: "React",
-            alternative0: <Alternative number="0" id="alternative0"/>,
-            alternative1: <Alternative number="1" id="alternative1"/>,
-            alternative2: <Alternative number="2" id="alternative2"/>,
-            alternative3: <Alternative number="3" id="alternative3"/>,
-            alternative4: <Alternative number="4" id="alternative4"/>
+            alternatives: [
+                <Alternative number="0" id="alternative0"/>,
+                <Alternative number="1" id="alternative1"/>,
+                <Alternative number="2" id="alternative2"/>,
+                <Alternative number="3" id="alternative3"/>,
+                <Alternative number="4" id="alternative4"/>
+            ]
         }
     }
 
     componentDidMount() {
         const id = localStorage.getItem("choiceID");
-        var xmlhttp = new XMLHttpRequest();
+        let xmlhttp = new XMLHttpRequest();
         const postTo = "https://oncs4wp3hd.execute-api.us-east-1.amazonaws.com/beta/choice";
         xmlhttp.open("POST", postTo, true);
         xmlhttp.responseType = "json";
@@ -38,72 +40,24 @@ class ViewChoice extends React.Component {
                 document.getElementById("choiceDescription").innerText = JSON.parse(xmlhttp.response.response)["Description"];
                 let alternatives = JSON.parse(xmlhttp.response.response)["Alternatives"];
                 for (let i = 0; i < 5; i++) {
+                    let tempAlternatives = this.state.alternatives;
                     if (alternatives[i]["description"] === "") {
-                        switch (i) {
-                            case 0:
-                                this.setState({alternative0: false});
-                                break;
-                            case 1:
-                                this.setState({alternative1: false});
-                                break;
-                            case 2:
-                                this.setState({alternative2: false});
-                                break;
-                            case 3:
-                                this.setState({alternative3: false});
-                                break;
-                            case 4:
-                                this.setState({alternative4: false});
-                                break;
-                            default:
-                                break;
-                        }
+                        tempAlternatives[i] = false;
+
                     } else {
                         document.getElementById("details" + i).innerText = alternatives[i]["description"];
-                        switch (i) {
-                            case 0:
-                                this.setState({
-                                    alternative0: <Alternative number="0" approvers={alternatives[i]["Approvers"]}
-                                                               disapprovers={alternatives[i]["Disapprovers"]}/>
-                                });
-                                break;
-                            case 1:
-                                this.setState({
-                                    alternative1: <Alternative number="1" approvers={alternatives[i]["Approvers"]}
-                                                               disapprovers={alternatives[i]["Disapprovers"]}/>
-                                });
-                                break;
-                            case 2:
-                                this.setState({
-                                    alternative2: <Alternative number="2" approvers={alternatives[i]["Approvers"]}
-                                                               disapprovers={alternatives[i]["Disapprovers"]}/>
-                                });
-                                break;
-                            case 3:
-                                this.setState({
-                                    alternative3: <Alternative number="3" approvers={alternatives[i]["Approvers"]}
-                                                               disapprovers={alternatives[i]["Disapprovers"]}/>
-                                });
-                                break;
-                            case 4:
-                                this.setState({
-                                    alternative4: <Alternative number="4" approvers={alternatives[i]["Approvers"]}
-                                                               disapprovers={alternatives[i]["Disapprovers"]}/>
-                                });
-                                break;
-                            default:
-                                break;
-                        }
+                        tempAlternatives[i] = <Alternative number={i.toString()}
+                                                           approvers={alternatives[i]["Approvers"]}
+                                                           disapprovers={alternatives[i]["Disapprovers"]}/>;
                     }
+                    this.setState({alternatives: tempAlternatives});
                 }
                 this.forceUpdate();
             }
         };
-
         const data = {
             uuidString: id
         }
-
         xmlhttp.send(JSON.stringify(data));
         console.log("local: " + JSON.stringify(data));
     }
@@ -121,15 +75,14 @@ class ViewChoice extends React.Component {
                     </Grid>
                     <Grid item xs={3}>
                         <Link to="/complete">
-                            <Button variant="contained" id="Complete" color="primary"
-                                    style={{float: "right"}}>Complete</Button>
+                            <Button variant="contained" id="Complete" color="primary">Complete</Button>
                         </Link>
                     </Grid>
-                    {this.state.alternative0}
-                    {this.state.alternative1}
-                    {this.state.alternative2}
-                    {this.state.alternative3}
-                    {this.state.alternative4}
+                    {this.state.alternative[0]}
+                    {this.state.alternative[1]}
+                    {this.state.alternative[2]}
+                    {this.state.alternative[3]}
+                    {this.state.alternative[4]}
                 </Grid>
             </div>
         );
